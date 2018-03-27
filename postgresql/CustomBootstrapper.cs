fbbbdc578c64fd8ef138e444de054c6c2a57f774
Tim Dallau <tim@dallau.com>
2018-03-27 06:36:16 +0200

    fixed the cors problem

---
+++ postgresql/CustomBootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Bootstrappers.Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace postgresql
+{
+    public class CustomBootstrapper : AutofacNancyBootstrapper {
+        protected override void ApplicationStartup(ILifetimeScope container, IPipelines pipelines)
+        {
+        pipelines.AfterRequest += ctx =>
+        {
+            ctx.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            ctx.Response.Headers.Add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+            ctx.Response.Headers.Add("Access-Control-Allow-Methods", "DELETE");
+        };
+        }
+    }
+}
